7a5d2223dea1ebdc35b9703d2afca2cf545cb822
lvminh97 <lvminh97@gmail.com>
2021-05-17 23:35:22 +0700

    Color mixing code

---
+++ Process.py
@@ -0,0 +1,161 @@
+from ColorParams import *
+import numpy as np
+import math
+
+class Helper:
+	BIT0 = 1
+	BIT1 = 2
+	BIT2 = 4
+	BIT3 = 8
+	BIT4 = 16
+	BIT5 = 32
+	BIT6 = 64
+	BIT7 = 128
+
+	def func(x):
+		if x > 0.008856:
+			return x ** (1 / 3)
+		else:
+			return (903.3 * x +16) / 116
+
+	def sampleToXYZ(sample, type = 0): # 0 -> 2* ; 1 -> 10*
+		if type == 0:
+			K = sum([Illuminant.D65_ILL[i] * ObserverFunction.Y2[i] for i in range(31)])
+			X = sum([Illuminant.D65_ILL[i] * ObserverFunction.X2[i] * sample[i] for i in range(31)])
+			Y = sum([Illuminant.D65_ILL[i] * ObserverFunction.Y2[i] * sample[i] for i in range(31)])
+			Z = sum([Illuminant.D65_ILL[i] * ObserverFunction.Z2[i] * sample[i] for i in range(31)])
+		else:
+			K = sum([Illuminant.D65_ILL[i] * ObserverFunction.Y10[i] for i in range(31)])
+			X = sum([Illuminant.D65_ILL[i] * ObserverFunction.X10[i] * sample[i] for i in range(31)])
+			Y = sum([Illuminant.D65_ILL[i] * ObserverFunction.Y10[i] * sample[i] for i in range(31)])
+			Z = sum([Illuminant.D65_ILL[i] * ObserverFunction.Z10[i] * sample[i] for i in range(31)])
+		return [X / K, Y / K, Z / K]
+
+	def rgbAdj(x):
+		res = 0
+		if x <= -0.0031308:
+			res = 0
+		elif x <= 0.0031308:
+			res = 12.92 * x
+		else:
+			res = 1.055 * (x ** 0.41666) - 0.055
+		if 0 <= res <= 1:
+			return res
+		elif res < 0:
+			return 0
+		else:
+			return 1
+
+	def sampleToRGB(sample):
+		X, Y, Z = Helper.sampleToXYZ(sample)
+		r = Helper.rgbAdj(3.2404542 * X - 1.5371385 * Y - 0.4985314 * Z)
+		g = Helper.rgbAdj(-0.969266 * X + 1.8760108 * Y + 0.0415560 * Z)
+		b = Helper.rgbAdj(0.0556434 * X - 0.2040259 * Y + 1.0572252 * Z)
+		return list(map(lambda x: round(x * 255), [r, g, b]))
+
+	def sampleToCIELAB(sample, type = 0):
+		X, Y, Z = Helper.sampleToXYZ(sample, type)
+		L = 116 * Helper.func(Y / Illuminant.D65_REF['Y']) - 16
+		a = 500 * (Helper.func(X / Illuminant.D65_REF['X']) - Helper.func(Y / Illuminant.D65_REF['Y']))
+		b = 200 * (Helper.func(Y / Illuminant.D65_REF['Y']) - Helper.func(Z / Illuminant.D65_REF['Z']))
+		return [L, a, b]
+
+	def sampleToCIELCH(sample):
+		L, a, b = Helper.sampleToCIELAB(sample)
+		C = (a * a + b * b) ** 0.5
+		if a != 0:
+			H = math.atan(b / a) / 3.1415926 * 180
+		else:
+			H = 90 if b >= 0 else 270
+		if a < 0:
+			H += 180
+		return [L, C, H]
+
+	def deltaE(Lab1, Lab2):
+		s = 0
+		for i in range(3):
+			s += (Lab1[i] - Lab2[i]) ** 2
+		return s ** 0.5
+
+	def deltaAB(Lab1, Lab2):
+		s = 0
+		for i in range(1, 3):
+			s += (Lab1[i] - Lab2[i]) ** 2
+		return s ** 0.5
+	def deltaH(Lch1, Lch2):
+		return abs(Lch1[2] - Lch2[2])
+	def deltaC(Lch1, Lch2):
+		return abs(Lch1[1] - Lch2[1])
+	def deltaCH(Lch1, Lch2):
+		s = 0
+		for i in range(1, 3):
+			s += (Lch1[i] - Lch2[i]) ** 2
+		return s ** 0.5
+
+class Process:
+	def __init__(self):
+		self.basicMat = []
+		self.ratio = []
+		self.minDiff = []
+		self.minDiff2 = []
+		self.ratioFinal = []
+		self.sampleFinal = []
+		self.LAB_Ref = []
+		self.len = 0
+
+	def backtrack(self, pos, bound, step, max):
+		for a in range(bound[pos][0], bound[pos][1] + 1, step):
+			if a < 0:
+				continue
+			elif sum(self.ratio[:pos]) + a <= max:
+				self.ratio[pos] = a
+				if pos < self.len - 2:
+					self.backtrack(pos + 1, bound, step, max)
+				elif sum(self.ratio[:self.len - 1]) < max:
+					self.ratio[self.len - 1] = max - sum(self.ratio[:self.len - 1])
+					ratioMat = np.matrix(list(map(lambda x: x / max, self.ratio))).T
+					sampleRes = self.basicMat * ratioMat
+					LAB_Act = Helper.sampleToCIELAB(sampleRes.T.tolist()[0])
+					# LAB_Act = Helper.sampleToCIELCH(sampleRes.T.tolist()[0])
+					# if Helper.deltaH(LAB_Act, self.LAB_Ref) < self.minDiff or (Helper.deltaH(LAB_Act, self.LAB_Ref) == self.minDiff and Helper.deltaC(LAB_Act, self.LAB_Ref) <= self.minDiff2):
+					# if Helper.deltaCH(LAB_Act, self.LAB_Ref) < self.minDiff:
+
+					if Helper.deltaAB(LAB_Act, self.LAB_Ref) < 5 and self.minDiff2 > abs(LAB_Act[0] - self.LAB_Ref[0]):
+						self.minDiff2 = abs(LAB_Act[0] - self.LAB_Ref[0])
+						self.ratioFinal = [i / max for i in self.ratio]
+						self.sampleFinal = sampleRes.T.tolist()[0]
+					elif Helper.deltaAB(LAB_Act, self.LAB_Ref) < self.minDiff:
+						self.minDiff = Helper.deltaCH(LAB_Act, self.LAB_Ref)
+						self.ratioFinal = [i / max for i in self.ratio]
+						self.sampleFinal = sampleRes.T.tolist()[0]
+			else:
+				return
+
+	def compute(self, basicColorChosen, sample, bound, step, max = 100):
+		basic = []
+		if basicColorChosen & Helper.BIT0:
+			basic += [BasicColor.YELLOW]
+		if basicColorChosen & Helper.BIT1:
+			basic += [BasicColor.RED]
+		if basicColorChosen & Helper.BIT2:
+			basic += [BasicColor.RH_RED]
+		if basicColorChosen & Helper.BIT3:
+			basic += [BasicColor.PINK]
+		if basicColorChosen & Helper.BIT4:
+			basic += [BasicColor.BLUE]
+		if basicColorChosen & Helper.BIT5:
+			basic += [BasicColor.BLACK]
+		self.len = len(basic)
+		self.basicMat = np.matrix(basic).T
+		self.LAB_Ref = Helper.sampleToCIELAB(sample)
+		# self.LAB_Ref = Helper.sampleToCIELCH(sample)
+
+		self.minDiff = 999999
+		self.minDiff2 = 999999
+		self.ratio = [0] * self.len
+		self.ratioFinal = []
+		self.sampleFinal = []
+
+		self.backtrack(0, bound, step, max)
+		return [self.ratioFinal, self.sampleFinal]
+
---
+++ main.py
@@ -0,0 +1,206 @@
+from PyQt5 import QtGui, QtWidgets, QtCore
+from PyQt5.QtCore import QDir, QSize, QSizeF, Qt, QUrl
+from PyQt5.QtGui import QTransform, QIcon
+from PyQt5.QtWidgets import (QApplication, QFileDialog, QFrame,
+        QLineEdit, QPlainTextEdit, QLabel, 
+        QPushButton, QSlider, QStyle, QVBoxLayout,
+        QWidget, QTableWidget, QTableWidgetItem,
+        QMessageBox, QCheckBox, QGroupBox)
+import os
+import sys
+import warnings
+from PlotCanvas import PlotCanvas
+from Process import *
+
+class App(QWidget):
+    def __init__(self):
+        super().__init__()
+
+        self.sampleData = []
+        self.ratio = [0] * 6
+
+        self.initUI()
+        
+    def initUI(self):
+        self.setWindowTitle("Công cụ tính toán pha màu")
+        self.setGeometry(60, 60, 1000, 650)
+        self.setFixedSize(1000, 650)
+
+        self.label1 = QLabel(self)
+        self.label1.setText("Các màu cơ bản")
+        self.label1.move(20, 20)
+
+        self.basicColor1CheckBox = QCheckBox("Yellow", self)
+        self.basicColor1CheckBox.resize(200, 20)
+        self.basicColor1CheckBox.move(20, 50)
+
+        self.basicColor2CheckBox = QCheckBox("Red", self)
+        self.basicColor2CheckBox.resize(200, 20)
+        self.basicColor2CheckBox.move(20, 80)
+
+        self.basicColor3CheckBox = QCheckBox("White", self)
+        self.basicColor3CheckBox.resize(200, 20)
+        self.basicColor3CheckBox.move(20, 110)
+
+        self.basicColor4CheckBox = QCheckBox("Magenta", self)
+        self.basicColor4CheckBox.resize(200, 20)
+        self.basicColor4CheckBox.move(140, 50)
+
+        self.basicColor5CheckBox = QCheckBox("Blue", self)
+        self.basicColor5CheckBox.resize(200, 20)
+        self.basicColor5CheckBox.move(140, 80)
+
+        self.basicColor6CheckBox = QCheckBox("Black", self)
+        self.basicColor6CheckBox.resize(200, 20)
+        self.basicColor6CheckBox.move(140, 110)
+
+        self.importSampleBtn = QPushButton("Nhập màu mẫu", self)
+        self.importSampleBtn.resize(100, 30)
+        self.importSampleBtn.move(20, 160)
+        self.importSampleBtn.clicked.connect(self.importSample)
+
+        self.sampleColorTable = QTableWidget(self)
+        self.sampleColorTable.resize(180, 400)
+        self.sampleColorTable.move(20, 200)
+        self.sampleColorTable.setRowCount(31)
+        self.sampleColorTable.setColumnCount(2)
+        self.sampleColorTable.setColumnWidth(0, 70)
+        self.sampleColorTable.setColumnWidth(1, 90)
+        self.sampleColorTable.verticalHeader().setVisible(False)
+        self.sampleColorTable.setHorizontalHeaderLabels(["Bước sóng", "Hệ số phản xạ"])
+        self.sampleColorTable.horizontalHeader().setStyleSheet("::section{font-weight: bold; background-color: #eee}")
+        wvl = 400
+        for i in range(31):
+            self.sampleColorTable.setItem(i, 0, QTableWidgetItem(str(wvl)))
+            wvl += 10
+
+        self.computeBtn = QPushButton(self)
+        self.computeBtn.setIcon(QIcon('run.png'))
+        self.computeBtn.setIconSize(QSize(25, 25))
+        self.computeBtn.resize(40, 120)
+        self.computeBtn.move(210, 200)
+        self.computeBtn.clicked.connect(self.compute)
+
+        self.ratioGroupBox = QGroupBox('Tỷ lệ pha màu', self)
+        self.ratioGroupBox.resize(250, 200)
+        self.ratioGroupBox.move(320, 20)
+        self.ratioText = QLabel(self)
+        self.ratioText.resize(200, 180)
+        self.ratioText.move(350, 30)
+        self.ratioText.setStyleSheet("font-weight:bold;")
+        self.printRatio()
+
+        self.sampleColorLabel = QLabel('Màu mẫu', self)
+        self.sampleColorLabel.move(650, 50)
+        self.sampleColorBox = QLabel(self)
+        self.sampleColorBox.resize(70, 20)
+        self.sampleColorBox.move(730, 48)
+        self.sampleColorBox.setStyleSheet('border: 1px solid #a9a8b3')
+        self.sampleColorCIELAB = QLabel('CIELAB: ', self)
+        self.sampleColorCIELAB.resize(180, 40)
+        self.sampleColorCIELAB.move(820, 36)
+
+        self.computeColorLabel = QLabel('Màu pha được', self)
+        self.computeColorLabel.move(650, 120)
+        self.computeColorBox = QLabel(self)
+        self.computeColorBox.resize(70, 20)
+        self.computeColorBox.move(730, 118)
+        self.computeColorBox.setStyleSheet('border: 1px solid #a9a8b3')
+        self.computeColorCIELAB = QLabel('CIELAB: ', self)
+        self.computeColorCIELAB.resize(180, 40)
+        self.computeColorCIELAB.move(820, 106)
+
+        self.graphView = PlotCanvas(self, width = 6, height = 3.5)
+        self.graphView.move(320, 250)
+        self.graphView.plot(list(range(400, 701, 10)), [1] + [0] * 30, 'w')
+
+        self.show()
+
+    def importSample(self):
+        options = QFileDialog.Options()
+        file, _ = QFileDialog.getOpenFileName(self, "Chọn tệp tin chứa thông số màu (.txt)", "./.",
+                                               "All Files(*)", options = options)
+        try:
+            if file != '':
+                self.sampleData = []
+                fp = open(file, 'r')
+                line = fp.readline()
+                while line:
+                    self.sampleData += [float(line)]
+                    line = fp.readline()
+                for i in range(31):
+                    self.sampleColorTable.setItem(i, 1, QTableWidgetItem(str(self.sampleData[i])))
+        except:
+            QMessageBox.about(self, 'Lỗi', 'File đầu vào sai định dạng dữ liệu. Hãy thử lại!')
+
+    def setSampleData(self):
+        self.sampleData = [0 for i in range(31)] 
+        for i in range(31):
+            self.sampleData[i] = float(self.sampleColorTable.item(i, 1).text())
+
+    def compute(self):
+        basicColorChosen = ((self.basicColor1CheckBox.isChecked()) 
+                        + (self.basicColor2CheckBox.isChecked() << 1)
+                        + (self.basicColor3CheckBox.isChecked() << 2)
+                        + (self.basicColor4CheckBox.isChecked() << 3)
+                        + (self.basicColor5CheckBox.isChecked() << 4)
+                        + (self.basicColor6CheckBox.isChecked() << 5))
+        self.setSampleData()                
+        process = Process()
+        Max = 5000
+        resp  = process.compute(basicColorChosen, self.sampleData, [[0, Max]] * 6, 1, Max)
+        # tmp  = process.compute(basicColorChosen, self.sampleData, [[tmp[0][i] - 15, tmp[0][i] + 15] for i in range(len(tmp[0]))], 5)
+        # resp = process.compute(basicColorChosen, self.sampleData, [[tmp[0][i] - 8, tmp[0][i] + 8] for i in range(len(tmp[0]))], 1)
+        sampleRes = resp[1]
+        ratio = resp[0]
+        pos = 0
+        self.ratio = [0, 0, 0, 0, 0, 0]
+        if self.basicColor1CheckBox.isChecked():
+            self.ratio[0] = ratio[pos]
+            pos += 1
+        if self.basicColor2CheckBox.isChecked():
+            self.ratio[1] = ratio[pos]
+            pos += 1
+        if self.basicColor3CheckBox.isChecked():
+            self.ratio[2] = ratio[pos]
+            pos += 1
+        if self.basicColor4CheckBox.isChecked():
+            self.ratio[3] = ratio[pos]
+            pos += 1
+        if self.basicColor5CheckBox.isChecked():
+            self.ratio[4] = ratio[pos]
+            pos += 1
+        if self.basicColor6CheckBox.isChecked():
+            self.ratio[5] = ratio[pos]
+            pos += 1
+        self.printRatio()
+        self.graphView.clear()
+        self.graphView.plot(list(range(400, 701, 10)), self.sampleData, 'b')
+        self.graphView.plot(list(range(400, 701, 10)), sampleRes, 'r')
+
+        self.setColorBox(0, Helper.sampleToRGB(self.sampleData))
+        self.setColorBox(1, Helper.sampleToRGB(sampleRes))
+
+        LAB1 = Helper.sampleToCIELAB(self.sampleData)
+        LAB2 = Helper.sampleToCIELAB(sampleRes)
+        self.sampleColorCIELAB.setText('CIELAB: (%.2f, %.2f, %.2f)' % (LAB1[0], LAB1[1], LAB1[2]))
+        self.computeColorCIELAB.setText('CIELAB: (%.2f, %.2f, %.2f)' % (LAB2[0], LAB2[1], LAB2[2]))
+
+        QMessageBox.about(self, 'Thông báo', 'Đã tính toán xong!')
+
+    def printRatio(self):
+        self.ratioText.setText("Yellow: {:.2f} %\n\nRed: {:.2f} %\n\nWhite: {:.2f} %\n\nMagenta: {:.2f} %\n\nBlue: {:.2f} %\n\nBlack: {:.2f} %".format(self.ratio[0] * 100, self.ratio[1] * 100, self.ratio[2] * 100, self.ratio[3] * 100, self.ratio[4] * 100, self.ratio[5] * 100))
+
+    def setColorBox(self, box, RGB): ## box = 0 -> sampleColor; box = 1 -> computeColor
+        styleSheet = 'border: 1px solid #a9a8b3; background-color: #' + '%02x%02x%02x' % (RGB[0], RGB[1], RGB[2])   
+        if box == 0:
+            self.sampleColorBox.setStyleSheet(styleSheet)
+        else:
+            self.computeColorBox.setStyleSheet(styleSheet)     
+
+if __name__ == '__main__':
+    os.system("color 0a")
+    warnings.filterwarnings('ignore') 
+    app = QApplication(sys.argv)
+    exe = App()
+    sys.exit(app.exec_())
